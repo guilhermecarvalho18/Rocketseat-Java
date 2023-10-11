4f6b79ba27d490a6bcae679c9642d3002ec6a051
guilhermecarvalho18 <guilhermecarvalho181201@gmail.com>
2023-10-10 23:57:07 -0300

    Setup table to store data Setup Interface for the IUserRepository

--- todolist/src/main/java/br/com/todolist/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/todolist/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.todolist.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,8 +26,22 @@ public class UserController {
      * Date
      * void
      */
+
+    @Autowired 
+    private IUserRepository userRepository;
+
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getUsername());        
+    public UserModel create(@RequestBody UserModel userModel){
+        //System.out.println(userModel.getUsername());  
+        var user = this.userRepository.findByUsername(userModel.getUsername());      
+        
+        if(user != null){
+            System.out.println("User already registered");
+            return null;
+        }
+        
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
